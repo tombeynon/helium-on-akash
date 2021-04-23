c39136aa5d2f3d2881c1ac987e0e806da76aa028
Tom Beynon <tom@tombeynon.co.uk>
2021-04-23 21:03:34 +0100

    Comments and disable miner upgrade

--- boot.sh
+++ boot.sh
@@ -5,15 +5,20 @@ export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
 aws s3 cp s3://$S3_KEY_PATH /var/data/miner/swarm_key
 
+# Stop on error
+set -e
+
 if [ ! -f "/var/data/miner/swarm_key" ]; then
   # Swarm key doesn't exist yet
   echo "Swarm key will be uploaded to S3"
   UPLOAD_KEY=1
 fi
 
-if [ ! -z $HELIUM_VERSION ]; then
-  miner upgrade $HELIUM_VERSION
-fi
+# This could upgrade the miner to a specific version...
+# but I can't be sure it does that/works correctly so don't want to rely on it
+#if [ ! -z $HELIUM_VERSION ]; then
+#  miner upgrade $HELIUM_VERSION
+#fi
 
 echo "Starting miner..."
 miner start && miner print_keys
